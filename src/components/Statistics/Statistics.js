45f45387847ca5ba1a714c52f470d3c0198f9417
ajfahim <ajfahim52@gmail.com>
2022-10-12 23:55:34 +0600

    chart added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
+    const data = useLoaderData();
+
     return (
-        <div className='text-white'>
-            Statistics
+        <div className='mt-20 flex justify-center'>
+
+            <LineChart
+                width={1000}
+                height={500}
+                data={data.data}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis dataKey="total" />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="total" stroke="#8884d8" />
+
+            </LineChart>
+
         </div>
     );
 };
